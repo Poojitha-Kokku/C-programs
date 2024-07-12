83188725e594aff3d82cb82dda82e8b94a1f6f8d
Poojitha-Kokku <knagapoojitha02@gmail.com>
2024-07-12 22:37:53 +0530

    Create 3.Sum of N Natural Numbers.c

---
+++ 3.Sum of N Natural Numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main() {
+    int i,n,sum;
+    printf("n:");
+    scanf("%d",&n);
+    sum=0;
+    for(i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+    printf("The sum is %d",sum);
+}
