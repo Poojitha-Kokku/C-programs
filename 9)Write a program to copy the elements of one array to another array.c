9aff86cc22e57c1cdb741a9f0196d58bf77d9c43
Poojitha-Kokku <knagapoojitha02@gmail.com>
2024-07-16 10:31:38 +0530

    Create 9)Write a program to copy the elements of one array to another array.c

---
+++ 9)Write a program to copy the elements of one array to another array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    int i;
+    int array1[4]={1,2,3,4};
+    int array2[i];
+    for(i=0;i<4;i++)
+    {
+        array2[4]=array1[i];
+    }
+    for(i=0;i<4;i++)
+    {
+        printf("%d",array2[i]);
+    }
+}
